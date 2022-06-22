073b226622cbfec7f5c5abff4b93bb1f97b9b315
Rômulo Victor <romulo.victor@unity3d.com>
2022-06-22 17:45:25 -0400

    adjusted CloudBuildHelper

--- Assets/Editor/CloudBuildHelper.cs
+++ Assets/Editor/CloudBuildHelper.cs
@@ -1,4 +1,3 @@
-#if UNITY_CLOUD_BUILD
 using UnityEngine;
 using System;
 using System.IO;
@@ -13,11 +12,12 @@ using UnityEditor.AddressableAssets.Settings;
 
 using Unity.Notifications;
 
-public class CloudBuildHelper : MonoBehaviour
+public static class CloudBuildHelper
 {
+#if UNITY_CLOUD_BUILD
     public static void PreExportDevelopment(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
-        Debug.Log("executed dev pre-export");
+        Debug.Log($"---> The {nameof(CloudBuildHelper)}.{nameof(PreExportDevelopment)} script is running.");
     }
 
     public static void PreExportEnterprise(UnityEngine.CloudBuild.BuildManifestObject manifest)
@@ -31,41 +31,5 @@ public class CloudBuildHelper : MonoBehaviour
     public static void PreExportQA(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
     }
-
-    private static T LoadScriptableObject<T>() where T : ScriptableObject
-    {
-        Type scriptableObjectType = typeof(T);
-        T scriptableObject = default(T);
-        string[] guids = AssetDatabase.FindAssets("t:" + scriptableObjectType);
-        if (guids.Length >= 1)
-        {
-            if (guids.Length > 1)
-                Debug.LogWarning("Found more than 1 " + scriptableObjectType + " in the project! Returning the first");
-
-            string guidToLoad = guids[0];
-            string path = AssetDatabase.GUIDToAssetPath(guidToLoad);
-            scriptableObject = AssetDatabase.LoadAssetAtPath<T>(path);
-        }
-        else
-        {
-            Debug.LogWarning("Couldn't find any " + typeof(T) + " in the project, trying through Resources...");
-
-            UnityEngine.Object[] vObjects = Resources.FindObjectsOfTypeAll(scriptableObjectType);
-            if (vObjects.Length >= 1)
-            {
-                if (vObjects.Length > 1)
-                    Debug.LogWarning("Found more than 1 " + scriptableObjectType + " in the project! Returning the first");
-
-                scriptableObject = (T)vObjects[0];
-            }
-            else
-            {
-                Debug.LogError("Couldn't find any " + scriptableObjectType + " through Resources either!");
-            }
-
-        }
-        
-        return scriptableObject;
-    }
-}
 #endif
+}
