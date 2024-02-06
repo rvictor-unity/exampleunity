099996cf6b14441441e8943585ddfa2e8cb1f017
Romulo Victor <romulo.victor@unity3d.com>
2024-02-06 14:27:08 -0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-msiexec /i https://awscli.amazonaws.com/AWSCLIV2.msi
-export PATH=$PATH:"C:\Program Files\Amazon\AWSCLIV2"
-# brew install awscli
-
-aws --version
-# python --version
-
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# python get-pip.py
-
-# pip --version
+echo "============ build.json ================"
+cat $WORKSPACE/build.json
+echo "========= end of build.json ============"
